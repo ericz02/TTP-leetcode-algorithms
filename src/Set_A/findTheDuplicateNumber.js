3c0ff64222e0bc3adcadd4dc324db6a945afc1dc
ericz02 <ezheng0720@gmail.com>
2023-07-09 22:29:52 -0400

    adds findTheDuplicateNumber

---
+++ src/Set_A/findTheDuplicateNumber.js
@@ -0,0 +1,20 @@
+var findDuplicates = function(nums) {
+  const duplicates = [];
+
+  let i = 0;
+  while (i < nums.length) {
+      if (nums[i] !== nums[nums[i] - 1]) {
+          [nums[i], nums[nums[i] - 1]] = [nums[nums[i] - 1], nums[i]];
+      } else {
+          i++;
+      }
+  }
+
+   for (let j = 0; j < nums.length; j++) {
+      if (nums[j] !== j + 1) {
+          duplicates.push(nums[j]);
+      }
+  }
+
+  return duplicates;
+};
---
+++ src/Set_A/findTheDuplicateNumber.test.js
@@ -0,0 +1,14 @@
+const findDuplicate = require('./main');
+
+describe('Find The Duplicate Number', () => {
+  test('Example 1', () => {
+    const nums = [1, 3, 4, 2, 2];
+    expect(findDuplicate(nums)).toBe(2);
+  });
+
+  test('Example 2', () => {
+    const nums = [3, 1, 3, 4, 2];
+    expect(findDuplicate(nums)).toBe(3);
+  });
+
+});
