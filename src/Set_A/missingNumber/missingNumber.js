49b765f1d9af6d66451ff24d28f277bbfc2efdf6
ericz02 <ezheng0720@gmail.com>
2023-07-10 15:31:28 -0400

    fixed while loop in missingNumber

--- src/Set_A/missingNumber/missingNumber.js
+++ src/Set_A/missingNumber/missingNumber.js
@@ -3,14 +3,11 @@ function missingNumber(nums) {
 
   // Iterate through the array
   while (i < nums.length) {
-    // If the current number is not equal to its index and it is within the range of the array,
-    // swap it with the number at its correct index
-    if (nums[i] !== i && nums[i] < nums.length) {
-      [nums[i], nums[nums[i]]] = [nums[nums[i]], nums[i]];
-    } else {
-      // Otherwise, move to the next index
+    const correctIndex = nums[i];
+    if (nums[i] !== nums[correctIndex])
+      [nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]];
+    else
       i++;
-    }
   }
 
   // Iterate over the array again to find the missing number
